2e3646c120a44ddccdf3710426e6dcb64a58e490
pas8 <whyiampas@gmail.com>
2021-11-22 09:22:23 +0200

    added email hook and some client js code

--- cartridges/app_onboarding/cartridge/client/default/js/product/notifyMe.js
+++ cartridges/app_onboarding/cartridge/client/default/js/product/notifyMe.js
@@ -3,12 +3,31 @@
 
 
 
-$('.select-size').on('change', function (e) {
-  e.preventDefault()
+$('.select-size').on('change', (__) => {
+  __.preventDefault()
   var notifyMeDialog = $('#notifyMeDialog')
   notifyMeDialog.show()
   notifyMeDialog.addClass('show')
-  
+
   document.querySelector('.modal-background').style.display = 'block'
 
+
 });
+
+$('#notifyMeDialogForm').submit(function (e) {
+  var form = $(this);
+  e.preventDefault();
+  var url = form.attr('action');
+  form.spinner().start();
+
+  $('.reset-password-form').trigger('login:register', e);
+  $.ajax({
+    url: url,
+    type: 'post',
+    dataType: 'json',
+    data: form.serialize(),
+    success: function (data) {
+      console.llog(data)
+    }
+  })
+})
--- cartridges/app_onboarding/cartridge/controllers/NotifyMe.js
+++ cartridges/app_onboarding/cartridge/controllers/NotifyMe.js
@@ -9,6 +9,7 @@ var server = require('server');
 server.post('Validate', function (req, res, next) {
   var notifyMeForm = server.forms.getForm('notifyMe');
   var isValid = notifyMeForm.valid
+  dw.system.HookMgr.callHook('notifyMe.email', 'send', notifyMeForm.email.value);
   // if() return 
   res.json({
     isValid: isValid
