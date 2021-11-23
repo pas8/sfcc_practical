a080c468dfa96116e3b5bc677395d06f3f2978eb
pas8 <whyiampas@gmail.com>
2021-11-23 11:41:45 +0200

    some code refactoring to normal view

--- cartridges/app_onboarding/cartridge/controllers/NotifyMe.js
+++ cartridges/app_onboarding/cartridge/controllers/NotifyMe.js
@@ -1,20 +1,18 @@
 'use strict';
 
-/**
- * @namespace Product
- */
 
 var server = require('server');
 
 server.post('Validate', function (req, res, next) {
   var notifyMeForm = server.forms.getForm('notifyMe');
   var isValid = notifyMeForm.valid
+
   dw.system.HookMgr.callHook('notifyMe.email', 'send', notifyMeForm.email.value);
-  // if() return 
   res.json({
     isValid: isValid
   })
-  // res.json('',{});
+
   next();
 })
+
 module.exports = server.exports();
