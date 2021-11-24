b24ec9d97e8331c6542a5fc67278d77fd307ac06
pas8 <whyiampas@gmail.com>
2021-11-24 10:35:00 +0200

    added productListItem and reset to only wishlist confg

--- cartridges/app_onboarding/cartridge/client/default/js/productDetails.js
+++ cartridges/app_onboarding/cartridge/client/default/js/productDetails.js
@@ -5,5 +5,4 @@ var processInclude = require('base/util');
 $(document).ready(function () {
     processInclude(require('base/product/detail'));
     processInclude(require('plugin_wishlists/product/wishlist'));
-    processInclude(require('plugin_giftregistry/product/giftRegistry'));
 });
---
+++ cartridges/app_onboarding/cartridge/models/productListItem.js
@@ -0,0 +1,37 @@
+'use strict'
+
+
+var base = module.superModule
+
+function productsListItem(productsListItemObj) {
+
+
+
+  var Site = require('dw/system/Site')
+  var Transaction = require('dw/system/Transaction')
+
+
+
+  base.call(this, productsListItemObj)
+  if (!productsListItemObj) return;
+
+  var ONE_DAY_MS = 24 * 60 * 60 * 1000
+  var LAST_MODIFIED_DATE = (Date.now() - productsListItemObj.lastModified.getTime()) / ONE_DAY_MS
+
+  var WISLIST_EXPIRATION_DATE = +(Site.current.getCustomPreferenceValue('expirationDaysOfwishlist'))
+
+  var daysLeft = WISLIST_EXPIRATION_DATE - LAST_MODIFIED_DATE
+
+  if (daysLeft < 0) {
+    Transaction.wrap(function () { productsListItemObj.list.removeItem(productsListItemObj) })
+    this.productsListItem = null
+    return;
+  }
+
+  this.productListItem.daysLeft = daysLeft
+
+
+}
+
+
+module.exports = productsListItem
