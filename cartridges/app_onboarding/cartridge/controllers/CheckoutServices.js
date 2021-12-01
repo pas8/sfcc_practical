e8261f374e05ceaa221baa3dccf9350ab1e9af8a
pas8 <whyiampas@gmail.com>
2021-12-01 11:14:08 +0200

    added basic custom object adding functionality and removed prev branch files

---
+++ cartridges/app_onboarding/cartridge/controllers/CheckoutServices.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+var server = require('server')
+
+
+
+server.extend(module.superModule)
+
+server.prepend('PlaceOrder', function (req, res, next) {
+  var COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
+  var TRANSACTION = require('dw/system/Transaction')
+  var BasketMgr = require('dw/order/BasketMgr');
+  var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+
+  var currentBasket = BasketMgr.getCurrentBasket();
+
+  var order = COHelpers.createOrder(currentBasket);
+
+  TRANSACTION.wrap(function () {
+    var CO  = CustomObjectMgr.createCustomObject('CustomOrder', order.orderNo)
+    CO.custom.productCount = currentBasket.allProductLineItems.length
+    CO.custom.email = currentBasket.customerEmail
+    CO.custom.firstAndLastName = currentBasket.customerName || currentBasket.customerEmail
+    CO.custom.totalPrice = order.totalGrossPrice.value
+  })
+  next()
+
+})
+
+
+
+module.exports = server.exports()
