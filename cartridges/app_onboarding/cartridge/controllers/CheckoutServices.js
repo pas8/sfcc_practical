88f5ebddf0e958e17394e0b27bac56c932ab94ae
pas8 <whyiampas@gmail.com>
2021-12-03 12:28:34 +0200

    updated file params and changes custom object order logic to orderMgr logic and also added metadata

--- cartridges/app_onboarding/cartridge/controllers/CheckoutServices.js
+++ cartridges/app_onboarding/cartridge/controllers/CheckoutServices.js
@@ -5,21 +5,24 @@ var server = require('server')
 
 server.extend(module.superModule)
 
-server.prepend('PlaceOrder', function (req, res, next) {
-  var COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
-  var TRANSACTION = require('dw/system/Transaction')
-  var BasketMgr = require('dw/order/BasketMgr');
-  var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+// server.prepend('PlaceOrder', function (req, res, next) {
+//   var COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
+//   var TRANSACTION = require('dw/system/Transaction')
+//   var BasketMgr = require('dw/order/BasketMgr');
+//   var OrderMgr = require('dw/order/OrderMgr');
+//   var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 
-  TRANSACTION.wrap(function () {
-    var CO  = CustomObjectMgr.createCustomObject('CustomOrder', order.orderNo)
-    CO.custom.productCount = currentBasket.allProductLineItems.length
-    CO.custom.email = currentBasket.customerEmail
-    CO.custom.firstAndLastName = currentBasket.customerName || currentBasket.customerEmail
-    CO.custom.totalPrice = order.totalGrossPrice.value
-  })
-  next()
-})
+//   TRANSACTION.wrap(function () {
+//     var CO = CustomObjectMgr.createCustomObject('CustomOrder', order.orderNo)
+//     CO.custom.productCount = currentBasket.allProductLineItems.length
+//     CO.custom.email = currentBasket.customerEmail
+//     CO.custom.firstAndLastName = currentBasket.customerName || currentBasket.customerEmail
+//     CO.custom.totalPrice = order.totalGrossPrice.value
+//   })
+  
+
+//   next()
+// })
 
 
 
